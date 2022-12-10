e4851bb791559d83bea7b3323edb1aec2b0f20d8
Vit <vitalii.tymashkov@gmail.com>
2022-12-10 13:01:40 +0000

    2

--- lab03/infra/main.tf
+++ lab03/infra/main.tf
@@ -7,4 +7,12 @@ module "network" {
 
   name_prefix = var.name_prefix
   address_space = local.address_space
+  subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+module "vm" {
+  source = "./modules/vm"
+
+  name_prefix = var.name_prefix
+  subnet_id = module.network.subnet_id
 }
